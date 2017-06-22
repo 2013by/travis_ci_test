065c83581c8997260ef8892b3038cee5bad0d7f6
2013by <980101386@qq.com>
2017-06-22 14:42:00 +0800

    add an example

---
+++ python/mytest.py
@@ -0,0 +1,13 @@
+'''
+Created on 2017-6-19
+
+@author: lizhu
+'''
+class Mytest:
+    def function1(self):
+        print "no argument and return"
+    def function2(self,a,b):
+        c=a+b
+    def function3(self,a,b):
+        c=a+b
+        return c
